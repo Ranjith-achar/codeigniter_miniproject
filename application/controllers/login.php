b6c08d070aa3f72ef37128314067914668244620
Ranjith-achar <ranjith.chandrachar@gmail.com>
2018-05-06 08:49:31 +0530

    validation

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -8,11 +8,29 @@ class Login extends MY_Controller{
      //$this->load->helper('url');
 		    $this->load->helper('form');
 		    $this->load->view('public/admin_login');
- }
+  }
 
 
 public function admin_login(){
-  echo "welcome to login";
+
+   $this->load->library('form_validation');
+	 $this->form_validation->set_rules('username','User Name','required|alpha|max_length[10]');
+	 $this->form_validation->set_rules('password', 'New Password','required|max_length[15]|min_length[6]|alpha_numeric');
+
+	 if ($this->form_validation->run())
+	 {
+			echo "success";
+	 }
+	 else
+	 {
+
+     //echo "validation failed";
+		 //
+		 $this->load->view('public/admin_login');
+	 }
+
+
+
 }
 
 
--- application/views/public/admin_login.php
+++ application/views/public/admin_login.php
@@ -4,15 +4,15 @@
  <legend>adminlogin</legend>
  <div class="form-group">
    <label class="control-label col-sm-2" for="email">Email:</label>
-   <div class="col-sm-10">
-     <input type="email" class="form-control" id="email" placeholder="Enter email">
-   </div>
+   <div class="col-sm-6">
+     <input type="text" class="form-control" name="username" placeholder="Enter username">
+   </div><?php echo form_error('username',"<p class='text-danger'>","</p>"); ?>
  </div>
  <div class="form-group">
    <label class="control-label col-sm-2" for="pwd">Password:</label>
-   <div class="col-sm-10">
-     <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-   </div>
+   <div class="col-sm-6">
+     <input type="password" class="form-control" name="password" placeholder="Enter password">
+   </div><?php echo form_error('password',"<p class='text-danger'>","</p>"); ?>
  </div>
  <div class="form-group">
    <div class="col-sm-offset-2 col-sm-10">
